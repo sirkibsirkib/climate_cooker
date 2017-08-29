214e036f62b947e8aa9bbd7f3a3e4bb7acceb285
sirkibsirkib <christopher.esterhuyse@gmail.com>
2017-08-29 13:55:15 +0200

    some nice changes here. beginnings of climate layer

--- helpful.py
+++ helpful.py
@@ -1,4 +1,4 @@
-from math import pi
+from math import pi, atan2
 import collections
 
 class Climate:
@@ -6,7 +6,113 @@ class Climate:
     HOT_STEPPE, COLD_DESERT, COLD_STEPPE, MARITIME_EAST_COAST,\
     MARITIME_WEST_COAST, MEDITERRANEAN,TEMPERATE_MONSOON,\
     LAUTENTIAN, SUBARCTIC, MANCHURIAN, SUBARCTIC_EAST,\
-    TUNDRA, ICECAP = range(17)
+    TUNDRA, ICECAP, WATER = range(18)
+
+    @staticmethod
+    def classify(s_temp, w_temp, s_prec, w_prec):
+        pass
+        #temp
+        # 0     50      100     140     170     200     250
+        #   v.cold  cold    mild    warm    hot     v.hot
+
+        #prec
+        # 0     10      20     40           70     170       250
+        #   dry     v.low   low     moderate  wet       v.wet
+
+        s_vhot = s_temp > 180
+        w_vhot = w_temp > 180
+        s_hot = s_temp > 170 and s_temp <= 255
+        w_hot = w_temp > 170 and w_temp <= 255
+        s_warm = s_temp > 100 and s_temp <= 200
+        w_warm = w_temp > 100 and w_temp <= 200
+        s_mild = s_temp > 60 and s_temp <= 180
+        w_mild = w_temp > 60 and w_temp <= 180
+        s_cold = s_temp > 20 and s_temp <= 100
+        w_cold = w_temp > 20 and w_temp <= 100
+        s_vcold = s_temp <= 55
+        w_vcold = w_temp <= 55
+
+        s_vwet = s_prec > 130
+        w_vwet = w_prec > 130
+        s_wet = s_prec > 50 and s_prec <= 220
+        w_wet = w_prec > 50 and w_prec <= 220
+        s_moderate = s_prec > 10 and s_prec <= 90
+        w_moderate = w_prec > 10 and w_prec <= 90
+        s_low = s_prec > 15 and s_prec <= 50
+        w_low = w_prec > 15 and w_prec <= 50
+        s_vlow = s_prec > 5 and s_prec <= 35
+        w_vlow = w_prec > 5 and w_prec <= 35
+        s_dry= s_prec <= 30
+        w_dry = w_prec <= 30
+
+
+        description = ''
+        if s_vhot: description += ' ' + 's_vhot'
+        if w_vhot: description += ' ' + 'w_vhot'
+        if s_hot: description += ' ' + 's_hot'
+        if w_hot: description += ' ' + 'w_hot'
+        if s_warm: description += ' ' + 's_warm'
+        if w_warm: description += ' ' + 'w_warm'
+        if s_mild: description += ' ' + 's_mild'
+        if w_mild: description += ' ' + 'w_mild'
+        if s_cold: description += ' ' + 's_cold'
+        if w_cold: description += ' ' + 'w_cold'
+        if s_vcold: description += ' ' + 's_vcold'
+        if w_vcold: description += ' ' + 'w_vcold'
+
+
+        if s_vwet: description += ' ' + 's_vwet'
+        if w_vwet: description += ' ' + 'w_vwet'
+        if s_wet: description += ' ' + 's_wet'
+        if w_wet: description += ' ' + 'w_wet'
+        if s_moderate: description += ' ' + 's_moderate'
+        if w_moderate: description += ' ' + 'w_moderate'
+        if s_low: description += ' ' + 's_low'
+        if w_low: description += ' ' + 'w_low'
+        if s_vlow: description += ' ' + 's_vlow'
+        if w_vlow: description += ' ' + 'w_vlow'
+        if s_dry: description += ' ' + 's_dry'
+        if w_dry: description += ' ' + 'w_dry'
+
+        if s_hot and w_hot and s_wet and w_wet:
+            return Climate.TROPICAL_RAINFOREST
+        elif s_hot and w_warm and s_vwet and w_dry:
+            return Climate.TROPICAL_MONSOON
+        elif s_hot and w_warm and s_wet and w_dry:
+            return Climate.SAVANNAH
+        elif s_vhot and w_warm and s_dry and w_dry:
+            return Climate.HOT_DESERT
+        elif s_hot and w_warm and (s_low or s_dry) and (w_low or w_dry):
+            return Climate.HOT_STEPPE
+        elif s_hot and w_cold and s_dry and w_dry:
+            return Climate.COLD_DESERT
+        elif s_warm and w_cold and (s_low or s_dry) and (w_low or w_dry):
+            return Climate.COLD_STEPPE
+        elif s_hot and (w_warm or w_mild) and s_wet and w_moderate:
+            return Climate.MARITIME_EAST_COAST
+        elif (s_warm or s_mild) and (w_mild or w_cold) and s_wet and w_wet:
+            return Climate.MARITIME_WEST_COAST
+        elif s_hot and w_mild and s_dry and w_moderate:
+            return Climate.MEDITERRANEAN
+        elif (s_hot or s_warm) and (w_mild or w_cold) and s_wet and w_dry:
+            return Climate.TEMPERATE_MONSOON
+        elif (s_warm or s_mild) and w_cold and s_moderate and w_low:
+            return Climate.LAUTENTIAN
+        elif (s_mild or s_cold) and w_vcold and s_moderate and w_vlow:
+            return Climate.SUBARCTIC
+        elif (s_warm or s_mild) and w_cold and s_moderate and w_dry:
+            return Climate.MANCHURIAN
+        elif (s_mild or s_cold) and w_vcold and s_moderate and w_dry:
+            return Climate.SUBARCTIC_EAST
+        elif s_cold and (w_cold or w_vcold) and s_low and w_dry:
+            return Climate.TUNDRA
+        elif s_vcold and w_vcold and (s_low or s_dry) and w_dry:
+            return Climate.ICECAP
+
+
+        print('couldnt classify temps:', s_temp, '/', w_temp, '   rain:', s_prec, '/', w_prec, description)
+        return -999
+
 
 def flatten(iterable):
     for el in iterable:
@@ -14,6 +120,7 @@ def flatten(iterable):
             yield from flatten(el)
         else:
             yield el
+
 
 def rel_color_func(x):
     if x == 0: return [0,0,0]
@@ -92,5 +199,7 @@ def climate_color_func(value):
         return [183, 190, 200]
     elif value == Climate.ICECAP:
         return [177, 247, 255]
-    else:
+    elif value == Climate.WATER:
         return [0, 0, 0]
+    else:
+        return [255, 0, 0]
--- main.py
+++ main.py
@@ -28,9 +28,23 @@ class Map:
         x, y = self.correct_coordinate(x, y)
         self.cells[y][x] = value
 
+    def increment(self, x, y, value):
+        x, y = self.correct_coordinate(x, y)
+        self.cells[y][x] += value
+
     def get(self, x, y):
         x, y = self.correct_coordinate(x, y)
         return self.cells[y][x]
+
+    def normalize(self, lower_bound, upper_bound, integers=True):
+        min_value = min(flatten(self.cells))
+        max_value = max(flatten(self.cells))
+        d_before = max_value - min_value
+        d_after = upper_bound - lower_bound
+        def normalize_aux(raw):
+            x = ((raw - min_value) * d_after / d_before) + lower_bound
+            return (int(x) if integers else x)
+        self.cells = [[normalize_aux(j) for j in i] for i in self.cells]
 
     @staticmethod
     def uniform_map(width, height, filling):
@@ -75,34 +89,36 @@ def pressure_map(rel_map, july=True):
 
 
     # put in interior pressure zones
-    for i in [2,3,4,6,7,8]:
-        y_pos = int(rel_map.height * i / 10)
-        for j in range(12):
-            x_pos = int(rel_map.width*j/12)
-            if rel_map.get(x_pos, y_pos) > 0 and \
-                            rel_map.get(x_pos, y_pos) > 0 and \
-                            rel_map.get(x_pos, y_pos) > 0 and \
-                            rel_map.get(x_pos, y_pos) > 0 and \
-                            rel_map.get(x_pos, y_pos) > 0:
-                if (i < 5 and july) or (i >= 5 and not july):
-                    low_nodes.append((x_pos, y_pos))
+    x_gap = ceil(rel_map.width / 23)
+    y_gap = ceil(rel_map.height / 19)
+    for j in range(19):
+        if j/19 < .1 or (j/19 > .45 and j/19 < .55) or j/19 > .9:
+            continue
+        for i in range(23):
+            int_x = int(rel_map.width * i / 23)
+            int_y = int(rel_map.height * j / 19)
+            if rel_map.get(int_x, int_y) > 0 and \
+                            rel_map.get(int_x+x_gap, int_y) > 0 and \
+                            rel_map.get(int_x-x_gap, int_y) > 0 and \
+                            rel_map.get(int_x, int_y+y_gap) > 0 and \
+                            rel_map.get(int_x, int_y-y_gap) > 0:
+                if (j/19 < .5 and july) or (j/19 > .5 and not july):
+                    low_nodes.append((int_x, int_y))
                 else:
-                    high_nodes.append((x_pos, y_pos))
+                    high_nodes.append((int_x, int_y))
 
 
     # put in pressure bands
     for i in [1,2,3,4,5]:
         relevant_list = low_nodes if i%2==1 else high_nodes
-        x_increment = ceil(rel_map.width / 14)
+        x_increment = ceil(rel_map.width / 47)
         x_pos = 1
+        # offsets the start position vertically by a small amount
+        flip_up = True
         y_axis = int(rel_map.height * i / 6 ) + season_offset
-
-
-
         while x_pos < rel_map.width:
-
-
-            y_walk = 0
+            y_walk = int(max_v_walk/2) if flip_up else -int(max_v_walk/2)
+            flip_up = not flip_up
             for w in range(max_v_walk):
                 if rel_map.get(x_pos, y_axis + y_walk) > 0:
                     y_walk += -1 if july else 1
@@ -137,16 +153,13 @@ def pressure_map(rel_map, july=True):
             return -((-press)**.3)
 
     pressure_cells = [
-        [
-            pressure_at(x, y) for x in range(rel_map.width)
-        ] for y in range(rel_map.height)
+        [pressure_at(x, y) for x in range(rel_map.width)] for y in range(rel_map.height)
     ]
-    min_press = min(flatten(pressure_cells))
-    max_press = max(flatten(pressure_cells))
-    def normalize(raw):
-        return int((raw - min_press) / (max_press - min_press) * 255)
-    normalized = [[normalize(j) for j in i] for i in pressure_cells]
-    return Map(normalized)
+    print(pressure_cells)
+    pre_map = Map(pressure_cells)
+    pre_map.normalize(0, 255, integers=True)
+    return pre_map
+
 
 def wind_map(pre_map):
     cells = []
@@ -157,8 +170,8 @@ def wind_map(pre_map):
             x_flow = pre_map.get(x-1 if x-1 >= 0 else pre_map.width-1, y) - pre_map.get((x+1)%pre_map.width, y)
             y_flow = pre_map.get(x, y-1 if y-1 >= 0 else pre_map.height-1) - pre_map.get(x, (y+1)%pre_map.height)
 
-            rot = atan2(y_flow, x_flow) + coriolis_rotation(y / pre_map.height)
-            power = sqrt(x_flow**2 + y_flow**2)
+            rot = 1.0 * atan2(y_flow, x_flow) + coriolis_rotation(y / pre_map.height)
+            power = sqrt(x_flow**2.0 + y_flow**2.0)
             max_power = max(max_power, power)
             row.append((rot, power))
         cells.append(row)
@@ -167,63 +180,58 @@ def wind_map(pre_map):
     ]
     return Map(cells)
 
+
+def rain_cloud(x, y, water, rel_map, win_map, rai_map, prev_height, depth_remaining):
+    this_height = max(1, rel_map.get(x, y))
+    (rot, pow) = win_map.get(x, y)
+    deposit_fraction = max(0.0, 1-((1-(pow**.9))**(1+this_height-prev_height)))
+    rai_map.increment(x, y, water*deposit_fraction)
+    water *= (1-deposit_fraction)
+
+    if depth_remaining == 0 or water < 0.03:
+        rai_map.increment(x, y, water) # dump remainder
+        return
+
+    angle_sets = [
+        (0.00*pi, 1, 0),    # right
+        (0.25*pi, 1, -1),   # up-right
+        (0.50*pi, 0, -1),   # up
+        (0.75*pi, -1, -1),  # up-left
+        (1.00*pi, -1, 0),   # left
+        (1.25*pi, -1, 1),   # down-left
+        (1.50*pi, 1, 0),    # down
+        (1.75*pi, 1, 1),    # down-right
+    ]
+
+    spent_shares = 0
+    recurse_clouds = []
+    for (angle, go_x, go_y) in angle_sets:
+        diff = min(
+            abs(rot - angle),
+            abs(rot - angle + pi*2),
+            abs(rot - angle - pi*2)
+        )
+        if diff > pi/2.3:
+            continue
+        share = 1 / (diff + 0.1)
+        spent_shares += share
+        recurse_clouds.append((share, go_x, go_y))
+    for (share, go_x, go_y) in recurse_clouds:
+        x2, y2 = rel_map.correct_coordinate(x + go_x, y + go_y)
+        rain_cloud(x2, y2, water*share/spent_shares, rel_map, win_map, rai_map, this_height, depth_remaining-1)
+
+
 def rain_map(rel_map, win_map):
     rai_map = Map.uniform_map_of_size(rel_map, 0)
     for y in range(rel_map.height):
         for x in range(rel_map.width):
-            water = 1.0
-            # TODO warm currents more water
-            prev_height = 0
             if rel_map.get(x, y) != 0:
                 # clouds start in the ocean
                 continue
+            rain_cloud(x, y, 1, rel_map, win_map, rai_map, 1, 10)
 
-            # simulate a rain cloud starting at position
-            cx, cy = x,y
-            for i in range(6):
-                (rot, pow) = win_map.get(cx, cy)
-                if rot < 0: rot += 2*pi
-
-                x_blown, y_blown = 0, 0
-                if rot > 5/8*pi and rot < 11/8*pi:
-                    x_blown = -1
-                elif rot < 3/8*pi or rot > 13/8*pi:
-                    x_blown = 1
-
-                if rot > 1/8*pi and rot < 7/8*pi:
-                    y_blown = -1
-                elif rot > 15/8*pi and rot < 15/8*pi:
-                    y_blown = 1
-
-                cx += x_blown
-                cy += y_blown
-
-                # wrap around globe, break at poles
-                if cx < 0: cx += rel_map.width
-                elif cx == rel_map.width: cx -= rel_map.width
-
-                if cy < 0 or cy == rel_map.height:
-                    break
-
-                new_height = rel_map.get(cx, cy)
-
-                # deposit some of stored water here as rain
-                deposit_fraction = max(0.0, 1-((.8)**(1+new_height-prev_height)))
-                prev_height = new_height
-                rai_map.set(cx, cy, min(10, rai_map.get(cx, cy) + water*deposit_fraction))
-                water -= water * deposit_fraction
-                if water < 0.26:
-                    break
-                if rel_map.get(cx, cy) == 0:
-                    water += 1
-
-    max_press = max(flatten(rai_map.cells))
-
-    def normalize(raw):
-        return int(raw / max_press * 255)
-
-    normalized = [[normalize(j) for j in i] for i in rai_map.cells]
-    return Map(normalized)
+    rai_map.normalize(0, 255, integers=True)
+    return rai_map
 
 def heat_map(rel_map, win_map, rai_map, july=True):
     cells = []
@@ -231,7 +239,7 @@ def heat_map(rel_map, win_map, rai_map, july=True):
         row = []
         for x in range(rel_map.width):
             heat_center = 0.4 if july else 0.6
-            sun_heat = 1.0 / (0.3 + abs(heat_center - (y/rel_map.height))) - 1.5
+            sun_heat = 1.0 / (0.25 + abs(heat_center - (y/rel_map.height))) - 1.3
             water_cooling = sum([
                 rel_map.get(x, y) == 0,
                 rel_map.get(x+1, y) == 0,
@@ -239,22 +247,14 @@ def heat_map(rel_map, win_map, rai_map, july=True):
                 rel_map.get(x-1, y) == 0,
                 rel_map.get(x, y-1) == 0,
             ])
-            if water_cooling > 0: water_cooling += 2
+            if water_cooling > 0: water_cooling += 1
 
             temp = (sun_heat - rel_map.get(x, y)/6) * 0.9**water_cooling
             row.append(temp)
         cells.append(row)
-
-    min_press = min(flatten(cells))
-    max_press = max(flatten(cells))
-
-    def normalize(raw):
-        return int((raw - min_press) / (max_press - min_press) * 255)
-
-    normalized = [[normalize(j) for j in i] for i in cells]
-    return Map(normalized)
-
-
+    hea_map = Map(cells)
+    hea_map.normalize(0, 255, integers=True)
+    return hea_map
 
 def climate_map(rel_map, rai_jul, rai_jan, hea_jul, hea_jan):
     cells = []
@@ -262,7 +262,19 @@ def climate_map(rel_map, rai_jul, rai_jan, hea_jul, hea_jan):
         row = []
         for x in range(rel_map.width):
             if rel_map.get(x, y) == 0:
+                row.append(Climate.WATER)
                 continue
+            classification = Climate.classify(hea_jul.get(x,y),
+                                              hea_jan.get(x,y),
+                                              rai_jul.get(x,y),
+                                              rai_jan.get(x,y)) if y < rel_map.height / 2 else \
+                    Climate.classify(hea_jan.get(x, y),
+                                     hea_jul.get(x, y),
+                                     rai_jan.get(x, y),
+                                     rai_jul.get(x, y))
+            row.append(classification)
+        cells.append(row)
+    return Map(cells)
 
 
 
@@ -287,9 +299,10 @@ rai_map_jul.render('05_jul_rain.png', gray_scale_color_func)
 rai_map_jan = rain_map(rel_map, win_map_jan)
 rai_map_jan.render('06_jan_rain.png', gray_scale_color_func)
 
-hea_map_jul = heat_map(rel_map, win_map_jul, rai_map_jul)
+hea_map_jul = heat_map(rel_map, win_map_jul, rai_map_jul, july=True)
 hea_map_jul.render('07_jul_heat.png', gray_scale_color_func)
-hea_map_jan = heat_map(rel_map, win_map_jan, rai_map_jan)
+hea_map_jan = heat_map(rel_map, win_map_jan, rai_map_jan, july=False)
 hea_map_jan.render('08_jan_heat.png', gray_scale_color_func)
 
-cli_map = climate_map
+cli_map = climate_map(rel_map, rai_map_jul, rai_map_jan, hea_map_jul, hea_map_jan)
+cli_map.render('09_climate.png', climate_color_func)
